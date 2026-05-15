276ed15ec5b4695f239db64b9694daee45e3bac0
Srikanthkrishnan <ks8283311@gmail.com>
2026-05-15 20:17:26 +0530

    Removed localhost MySQL config

--- php/config.php
+++ php/config.php
@@ -1,26 +1,3 @@
-<!-- <?php
-
-require '../vendor/autoload.php';
-
-$mysql = new mysqli(
-    "127.0.0.1",
-    "root",
-    "",
-    "internship_auth",
-    3307
-);
-
-if ($mysql->connect_error) {
-    die("Connection failed: " . $mysql->connect_error);
-}
-
-$mongoClient = new MongoDB\Client("mongodb://localhost:27017");
-
-$mongoDB = $mongoClient->internship_profile;
-
-$redis = new Predis\Client();
-
-?> -->
 <?php
 
 $host = getenv("MYSQLHOST");
@@ -29,14 +6,22 @@ $password = getenv("MYSQLPASSWORD");
 $database = getenv("MYSQLDATABASE");
 $port = getenv("MYSQLPORT");
 
-$conn = new mysqli($host, $user, $password, $database, $port);
+$conn = new mysqli(
+    $host,
+    $user,
+    $password,
+    $database,
+    $port
+);
 
 if ($conn->connect_error) {
 
-    die(json_encode([
+    echo json_encode([
         "status" => "error",
         "message" => $conn->connect_error
-    ]));
+    ]);
+
+    exit;
 }
 
 ?>
