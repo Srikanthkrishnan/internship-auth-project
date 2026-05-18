ac150371a1715db51617ecf12e0cf35bd0ba7ad8
Srikanthkrishnan <ks8283311@gmail.com>
2026-05-18 11:14:47 +0530

    Fixed Railway MySQL public connection

--- php/config.php
+++ php/config.php
@@ -5,14 +5,16 @@ require '../vendor/autoload.php';
 use MongoDB\Client;
 use Predis\Client as RedisClient;
 
-/* ================= MYSQL ================= */
+// ================= MYSQL =================
 
+// PUBLIC Railway MySQL Credentials
 $mysql_host = "yamabiko.proxy.rlwy.net";
 $mysql_port = 13373;
 $mysql_db   = "railway";
 $mysql_user = "root";
 $mysql_pass = "OZRrSkuwUDeYIDgohPfxmiXcSDwKtGuf";
 
+// Create MySQL connection
 $conn = new mysqli(
     $mysql_host,
     $mysql_user,
@@ -21,15 +23,16 @@ $conn = new mysqli(
     $mysql_port
 );
 
+// Check connection
 if ($conn->connect_error) {
     die("MySQL Connection Failed: " . $conn->connect_error);
 }
 
-/* ================= MONGODB ================= */
+// ================= MONGODB =================
+
+$mongo_uri = "mongodb+srv://srikanthkrishnan15_db_user:hpT87xGEEzR7zoIe@cluster0.p8n08ma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 try {
-
-    $mongo_uri = "mongodb+srv://srikanthkrishnan15_db_user:hpT87xGEEzR7zoIe@cluster0.p8n08ma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
     $mongoClient = new Client($mongo_uri);
 
@@ -40,13 +43,13 @@ try {
     die("MongoDB Connection Failed: " . $e->getMessage());
 }
 
-/* ================= REDIS ================= */
+// ================= REDIS =================
 
 $redis = new RedisClient([
-    "scheme" => "tcp",
-    "host" => "YOUR_REDIS_HOST",
-    "port" => 6379,
-    "password" => "gQAAAAAAAejtAAIgcDIwMTAzNTU1NjY4ZmQ0ODhhYjJhODg2YWRiOTM1OTAxYQ"
+    'scheme' => 'tcp',
+    'host'   => 'model-satyr-125165.upstash.io',
+    'port'   => 6379,
+    'password' => 'gQAAAAAAAejtAAIgcDIwMTAzNTU1NjY4ZmQ0ODhhYjJhODg2YWRiOTM1OTAxYQ'
 ]);
 
 ?>
